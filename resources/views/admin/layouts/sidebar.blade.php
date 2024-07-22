a5172918fee7aff8e7f334c8c9858b539efd3aac
Monel Lindu <monel.redlind@gmail.com>
2024-07-22 13:36:00 +0700

    updating sidebar icons

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -8,23 +8,23 @@
       </div>
       <ul class="sidebar-menu">
         <li class="menu-header">Dashboard</li>
-        <li class="active"><a class="nav-link" href="#"><i class="far fa-square"></i> <span>Home</span></a></li>
+        <li class="active"><a class="nav-link" href="#"><i class="fas fa-home"></i> <span>Home</span></a></li>
         <li class="dropdown">
-          <a href="#" class="nav-link has-dropdown"><i class="fas fa-fire"></i><span>Target</span></a>
+          <a href="#" class="nav-link has-dropdown"><i class="fas fa-bullseye"></i></i><span>Target</span></a>
           <ul class="dropdown-menu">
             <li class=''><a class="nav-link" href="index-0.html">General Dashboard</a></li>
             <li><a class="nav-link" href="#">Ecommerce Dashboard</a></li>
           </ul>
         </li>
         <li class="menu-header">Starter</li>
-        <li><a class="nav-link" href="{{route('admin.brand-client.index')}}"><i class="far fa-square"></i> <span>Brand/Client</span></a></li>
-        <li><a class="nav-link" href="{{route('admin.daily-report.index')}}"><i class="far fa-square"></i> <span>Daily Report</span></a></li>
-        <li><a class="nav-link" href="{{route('admin.proposal-surat.index')}}"><i class="far fa-square"></i> <span>Proposal dan Surat</span></a></li>
-        <li><a class="nav-link" href="{{route('admin.media-order.index')}}"><i class="far fa-square"></i> <span>List Media Order</span></a></li>
+        <li><a class="nav-link" href="{{route('admin.brand-client.index')}}"><i class="fas fa-phone"></i> <span>Brand/Client</span></a></li>
+        <li><a class="nav-link" href="{{route('admin.daily-report.index')}}"><i class="fas fa-vote-yea"></i> <span>Daily Report</span></a></li>
+        <li><a class="nav-link" href="{{route('admin.proposal-surat.index')}}"><i class="fas fa-mail-bulk"></i> <span>Proposal dan Surat</span></a></li>
+        <li><a class="nav-link" href="{{route('admin.media-order.index')}}"><i class="fas fa-tv"></i> <span>List Media Order</span></a></li>
 
         <li class="menu-header">Setting</li>
-        <li><a class="nav-link" href="{{route('admin.profile')}}"><i class="far fa-square"></i> <span>My Profile</span></a></li>
-        <li><a class="nav-link" href="#"><i class="far fa-square"></i> <span>User Settings</span></a></li>
+        <li><a class="nav-link" href="{{route('admin.profile')}}"><i class="fas fa-user-alt"></i> <span>My Profile</span></a></li>
+        <li><a class="nav-link" href="#"><i class="fas fa-user-cog"></i> <span>User Settings</span></a></li>
 
       <div class="mt-4 mb-4 p-3 hide-sidebar-mini">
         <a href="https://getstisla.com/docs" class="btn btn-primary btn-lg btn-block btn-icon-split">
