4074337471537fd148cfd669185653d1c9674db1
Daniel Christopher <mrdanielchristopher15@gmail.com>
2024-07-22 16:23:00 +0700

    Update index.blade.php
    Iseng test

--- resources/views/admin/brand-client/index.blade.php
+++ resources/views/admin/brand-client/index.blade.php
@@ -78,7 +78,7 @@
           </button>
         </div>
         <div class="modal-body">
-          ...
+          ...Test
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
