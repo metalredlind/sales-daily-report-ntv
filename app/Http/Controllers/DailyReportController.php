0b614f0929e9824784326ba01b10acb9d8be4694
Monel Lindu <monel.redlind@gmail.com>
2024-07-31 10:51:02 +0700

    editDailyReport

--- app/Http/Controllers/DailyReportController.php
+++ app/Http/Controllers/DailyReportController.php
@@ -70,7 +70,8 @@ class DailyReportController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $dailyReport = DailyReport::findOrFail($id);
+        return view('admin.daily-report.edit', compact('dailyReport'));
     }
 
     /**
@@ -78,7 +79,32 @@ class DailyReportController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            'waktu' => ['required'],
+            'tim_bertugas' => ['required', 'max:200'],
+            'nama_brand_klien' => ['max:200'],
+            'lokasi_pertemuan' => ['max:200'],
+            'nama_klien' => ['max:200'],
+            'nomor_telepon' => ['max:200'],
+            'jenis_kegiatan' => ['required', 'max:200'],
+            'follow_up' => ['required', 'max:200'],
+        ]);
+
+        $dailyReport = DailyReport::findOrfail($id);
+
+        $dailyReport->waktu = $request->waktu;
+        $dailyReport->tim_bertugas = $request->tim_bertugas;
+        $dailyReport->nama_brand_klien = $request->nama_brand_klien;
+        $dailyReport->lokasi_pertemuan = $request->lokasi_pertemuan;
+        $dailyReport->nama_klien = $request->nama_klien;
+        $dailyReport->nomor_telepon = $request->nomor_telepon;
+        $dailyReport->jenis_kegiatan = $request->jenis_kegiatan;
+        $dailyReport->follow_up = $request->follow_up;
+        $dailyReport->save();
+
+        toastr('Daily Report baru berhasil diupdate', 'success');
+
+        return redirect()->route('admin.daily-report.index');
     }
 
     /**
