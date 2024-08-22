2a3de504a8af70b0c5d97fc28a9727336c7bf83f
Monel Lindu <monel.redlind@gmail.com>
2024-08-22 23:03:24 +0700

    making helper and setActive for sidebar

--- resources/views/manager/layouts/sidebar.blade.php
+++ resources/views/manager/layouts/sidebar.blade.php
@@ -8,22 +8,16 @@
         </div>
         <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
-            <li class="active"><a class="nav-link" href="{{ route('manager.dashboard') }}"><i class="fas fa-home"></i> <span>Home</span></a></li>
-            <li><a class="nav-link" href="{{ route('manager.target-sales.index') }}"><i class="fas fa-bullseye"></i></i>
-            <span>Target</span></a></li>
+            <li class="{{setActive(['manager.dashboard.*'])}}"><a class="nav-link" href="{{ route('manager.dashboard') }}"><i class="fas fa-home"></i> <span>Home</span></a></li>
+            <li class="{{setActive(['manager.target-sales.*'])}}"><a class="nav-link" href="{{ route('manager.target-sales.index') }}"><i class="fas fa-bullseye"></i></i><span>Target</span></a></li>
             <li class="menu-header">Starter</li>
-            <li><a class="nav-link" href="{{ route('manager.brand-client.index') }}"><i class="fas fa-phone"></i>
-                    <span>Brand/Client</span></a></li>
-            <li><a class="nav-link" href="{{ route('manager.daily-report.index') }}"><i class="fas fa-vote-yea"></i>
-                    <span>Daily Report</span></a></li>
-            <li><a class="nav-link" href="{{ route('manager.proposal-surat.index') }}"><i class="fas fa-mail-bulk"></i>
-                    <span>Proposal dan Surat</span></a></li>
-            <li><a class="nav-link" href="{{ route('manager.media-order.index') }}"><i class="fas fa-tv"></i> <span>List
-                        Media Order</span></a></li>
+            <li class="{{setActive(['manager.brand-client.*'])}}"><a class="nav-link" href="{{ route('manager.brand-client.index') }}"><i class="fas fa-phone"></i><span>Brand/Client</span></a></li>
+            <li class="{{setActive(['manager.daily-report.*'])}}"><a class="nav-link" href="{{ route('manager.daily-report.index') }}"><i class="fas fa-vote-yea"></i><span>Daily Report</span></a></li>
+            <li class="{{setActive(['manager.proposal-surat.*'])}}"><a class="nav-link" href="{{ route('manager.proposal-surat.index') }}"><i class="fas fa-mail-bulk"></i><span>Proposal dan Surat</span></a></li>
+            <li class="{{setActive(['manager.media-order.*'])}}"><a class="nav-link" href="{{ route('manager.media-order.index') }}"><i class="fas fa-tv"></i> <span>List Media Order</span></a></li>
 
             <li class="menu-header">Setting</li>
-            <li><a class="nav-link" href="{{ route('manager.profile') }}"><i class="fas fa-user-alt"></i> <span>My
-                        Profile</span></a></li>
+            <li class="{{setActive(['manager.profile'])}}"><a class="nav-link" href="{{ route('manager.profile') }}"><i class="fas fa-user-alt"></i> <span>My Profile</span></a></li>
 
     </aside>
 </div>
--- resources/views/sales/layouts/sidebar.blade.php
+++ resources/views/sales/layouts/sidebar.blade.php
@@ -8,22 +8,17 @@
         </div>
         <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
-            <li class="active"><a class="nav-link" href="{{ route('sales.dashboard') }}"><i class="fas fa-home"></i> <span>Home</span></a></li>
-            <li><a class="nav-link" href="{{ route('sales.target-sales.index') }}"><i class="fas fa-bullseye"></i></i>
+            <li class="{{setActive(['sales.dashboard'])}}"><a class="nav-link" href="{{ route('sales.dashboard') }}"><i class="fas fa-home"></i> <span>Home</span></a></li>
+            <li class="{{setActive(['sales.target-sales.*'])}}"><a class="nav-link" href="{{ route('sales.target-sales.index') }}"><i class="fas fa-bullseye"></i></i>
             <span>Target</span></a></li>
             <li class="menu-header">Starter</li>
-            <li><a class="nav-link" href="{{ route('sales.brand-client.index') }}"><i class="fas fa-phone"></i>
-                    <span>Brand/Client</span></a></li>
-            <li><a class="nav-link" href="{{ route('sales.daily-report.index') }}"><i class="fas fa-vote-yea"></i>
-                    <span>Daily Report</span></a></li>
-            <li><a class="nav-link" href="{{ route('sales.proposal-surat.index') }}"><i class="fas fa-mail-bulk"></i>
-                    <span>Proposal dan Surat</span></a></li>
-            <li><a class="nav-link" href="{{ route('sales.media-order.index') }}"><i class="fas fa-tv"></i> <span>List
-                        Media Order</span></a></li>
+            <li class="{{setActive(['sales.brand-client.*'])}}"><a class="nav-link" href="{{ route('sales.brand-client.index') }}"><i class="fas fa-phone"></i><span>Brand/Client</span></a></li>
+            <li class="{{setActive(['sales.daily-report.*'])}}"><a class="nav-link" href="{{ route('sales.daily-report.index') }}"><i class="fas fa-vote-yea"></i><span>Daily Report</span></a></li>
+            <li class="{{setActive(['sales.proposal-surat.*'])}}"><a class="nav-link" href="{{ route('sales.proposal-surat.index') }}"><i class="fas fa-mail-bulk"></i><span>Proposal dan Surat</span></a></li>
+            <li class="{{setActive(['sales.media-order.*'])}}"><a class="nav-link" href="{{ route('sales.media-order.index') }}"><i class="fas fa-tv"></i> <span>List Media Order</span></a></li>
 
             <li class="menu-header">Setting</li>
-            <li><a class="nav-link" href="{{ route('sales.profile') }}"><i class="fas fa-user-alt"></i> <span>My
-                        Profile</span></a></li>
+            <li class="{{setActive(['sales.profile'])}}"><a class="nav-link" href="{{ route('sales.profile') }}"><i class="fas fa-user-alt"></i> <span>My Profile</span></a></li>
 
     </aside>
 </div>
