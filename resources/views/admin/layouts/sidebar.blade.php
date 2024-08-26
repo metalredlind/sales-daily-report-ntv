062802cb38d2fce75a087d7bc4f78ada95061cef
Monel Lindu <monel.redlind@gmail.com>
2024-08-26 11:12:57 +0700

    change text media order into paket

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -15,7 +15,7 @@
             <li class="{{setActive(['admin.brand-client.*'])}}"><a class="nav-link" href="{{ route('admin.brand-client.index') }}"><i class="fas fa-phone"></i><span>Brand/Client</span></a></li>
             <li class="{{setActive(['admin.daily-report.*'])}}"><a class="nav-link" href="{{ route('admin.daily-report.index') }}"><i class="fas fa-vote-yea"></i><span>Daily Report</span></a></li>
             <li class="{{setActive(['admin.proposal-surat.*'])}}"><a class="nav-link" href="{{ route('admin.proposal-surat.index') }}"><i class="fas fa-mail-bulk"></i><span>Proposal dan Surat</span></a></li>
-            <li class="{{setActive(['admin.media-order.*'])}}"><a class="nav-link" href="{{ route('admin.media-order.index') }}"><i class="fas fa-tv"></i> <span>List Media Order</span></a></li>
+            <li class="{{setActive(['admin.media-order.*'])}}"><a class="nav-link" href="{{ route('admin.media-order.index') }}"><i class="fas fa-tv"></i> <span>List Paket</span></a></li>
 
             <li class="menu-header">Setting</li>
             <li class="{{setActive(['admin.profile'])}}"><a class="nav-link" href="{{ route('admin.profile') }}"><i class="fas fa-user-alt"></i> <span>My Profile</span></a></li>
