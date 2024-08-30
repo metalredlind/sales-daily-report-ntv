3df65e80383d8372e56d77fc6ae55f420b2e03d9
Monel Lindu <monel.redlind@gmail.com>
2024-08-30 16:06:32 +0700

    some small fixes in seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -22,67 +22,7 @@ class UserSeeder extends Seeder
                 'title'=>'Administrator',
                 'team' => '0',
                 'status' => 'active',
-                'password' => bcrypt('password')
-            ],
-            [
-                'name' => 'Manager Test',
-                'username' => 'manager0',
-                'email' => 'manager.dailyreport@nusantaratv.com',
-                'role' => 'manager',
-                'title'=>'Manager Tim Test',
-                'team' => '0',
-                'status' => 'active',
-                'password' => bcrypt('password')
-            ],
-            [
-                'name' => 'Sales Test',
-                'username' => 'sales0',
-                'email' => 'sales.dailyreport@nusantaratv.com',
-                'role' => 'sales',
-                'title'=>'Sales Tim Test',
-                'team' => '0',
-                'status' => 'active',
-                'password' => bcrypt('password')
-            ],
-            [
-                'name' => 'Manager Team 1',
-                'username' => 'manager1',
-                'email' => 'manager1.dailyreport@nusantaratv.com',
-                'role' => 'manager',
-                'title'=>'Manager Tim 1',
-                'team' => '1',
-                'status' => 'active',
-                'password' => bcrypt('password')
-            ],
-            [
-                'name' => 'Sales Team 1',
-                'username' => 'sales1',
-                'email' => 'sales1.dailyreport@nusantaratv.com',
-                'role' => 'sales',
-                'title'=>'Sales Tim 1',
-                'team' => '1',
-                'status' => 'active',
-                'password' => bcrypt('password')
-            ],
-            [
-                'name' => 'Manager Team 2',
-                'username' => 'manager2',
-                'email' => 'manager2.dailyreport@nusantaratv.com',
-                'role' => 'manager',
-                'title'=>'Manager Tim 2',
-                'team' => '2',
-                'status' => 'active',
-                'password' => bcrypt('password')
-            ],
-            [
-                'name' => 'Sales Team 2',
-                'username' => 'sales2',
-                'email' => 'sales2.dailyreport@nusantaratv.com',
-                'role' => 'sales',
-                'title'=>'Sales Tim 2',
-                'team' => '2',
-                'status' => 'active',
-                'password' => bcrypt('password')
+                'password' => bcrypt('password@sales')
             ],
         ]);
     }
